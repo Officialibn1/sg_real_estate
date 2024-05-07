3d5c5cbc3fcf2136c9e08b9f188627eee6b6adf8
Isah Ibn Muhammad <officialibn001@gmail.com>
2024-05-07 02:09:22 +0100

    Footer complete

---
+++ src/components/Footer.svelte
@@ -0,0 +1,137 @@
+<footer>
+	<div class="section_container">
+		<div class="flex flex-col sm:flex-row flex-wrap gap-8 lg:justify-between lg:items-start w-full">
+			<div class="flex flex-col gap-5 lg:flex-1">
+				<div class="flex gap-3 items-center">
+					<i class="fa fa-home text-4xl lg:text-7xl font-bold" aria-hidden="true"></i>
+
+					<span class="text-2xl lg:text-4xl font-light"> S&G Estate. </span>
+				</div>
+
+				<p class="text-sm font-light leading-relaxed">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+					ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+					ullamco laboris nisi ut aliquip ex ea commodo consequat.
+				</p>
+
+				<div class="flex gap-6 item-center">
+					<span>
+						<i class="fa-brands text-2xl fa-facebook text-[#1877F2]"></i>
+					</span>
+
+					<span>
+						<i class="fa-brands text-2xl fa-instagram text-[#1DA1F2]"></i>
+					</span>
+
+					<span>
+						<i class="fa-brands text-2xl fa-x-twitter text-[#EC3397]"></i>
+					</span>
+
+					<span>
+						<i class="fa-brands text-2xl fa-tiktok text-[#110229]"></i>
+					</span>
+				</div>
+
+				<span class="text-sm font-medium opacity-50 text-[#110229]"
+					>© 2021 . All rights reserved.</span
+				>
+			</div>
+
+			<div class="flex gap-8 lg:flex-1 lg:justify-center">
+				<ul class="flex flex-col gap-4 capitalize">
+					<li><h1 class="mb-6 font-bold text-lg">Take a tour</h1></li>
+
+					<li class="relative group w-fit">
+						<span
+							class="hidden lg:flex lg:group-hover:w-full w-0 h-[2px] bg-gradient-to-r from-[#110229] to-[#8F90A6] ease-in-out duration-200 absolute -bottom-[2px] left-0"
+						></span>
+						<a href="/">Features</a>
+					</li>
+
+					<li class="relative group w-fit">
+						<span
+							class="hidden lg:flex lg:group-hover:w-full w-0 h-[2px] bg-gradient-to-r from-[#110229] to-[#8F90A6] ease-in-out duration-200 absolute -bottom-[2px] left-0"
+						></span>
+						<a href="/">Projects</a>
+					</li>
+					<li class="relative group w-fit">
+						<span
+							class="hidden lg:flex lg:group-hover:w-full w-0 h-[2px] bg-gradient-to-r from-[#110229] to-[#8F90A6] ease-in-out duration-200 absolute -bottom-[2px] left-0"
+						></span>
+						<a href="/">Partners</a>
+					</li>
+
+					<li class="relative group w-fit">
+						<span
+							class="hidden lg:flex lg:group-hover:w-full w-0 h-[2px] bg-gradient-to-r from-[#110229] to-[#8F90A6] ease-in-out duration-200 absolute -bottom-[2px] left-0"
+						></span>
+						<a href="/">Support</a>
+					</li>
+				</ul>
+
+				<ul class="flex flex-col gap-4 capitalize">
+					<li><h1 class="mb-6 font-bold text-lg">Our company</h1></li>
+
+					<li class="relative group w-fit">
+						<span
+							class="hidden lg:flex lg:group-hover:w-full w-0 h-[2px] bg-gradient-to-r from-[#110229] to-[#8F90A6] ease-in-out duration-200 absolute -bottom-[2px] left-0"
+						></span>
+						<a href="/">about us</a>
+					</li>
+
+					<li class="relative group w-fit">
+						<span
+							class="hidden lg:flex lg:group-hover:w-full w-0 h-[2px] bg-gradient-to-r from-[#110229] to-[#8F90A6] ease-in-out duration-200 absolute -bottom-[2px] left-0"
+						></span>
+						<a href="/">agents</a>
+					</li>
+
+					<li class="relative group w-fit">
+						<span
+							class="hidden lg:flex lg:group-hover:w-full w-0 h-[2px] bg-gradient-to-r from-[#110229] to-[#8F90A6] ease-in-out duration-200 absolute -bottom-[2px] left-0"
+						></span>
+						<a href="/">blog</a>
+					</li>
+
+					<li class="relative group w-fit">
+						<span
+							class="hidden lg:flex lg:group-hover:w-full w-0 h-[2px] bg-gradient-to-r from-[#110229] to-[#8F90A6] ease-in-out duration-200 absolute -bottom-[2px] left-0"
+						></span>
+						<a href="/">media</a>
+					</li>
+
+					<li class="relative group w-fit">
+						<span
+							class="hidden lg:flex lg:group-hover:w-full w-0 h-[2px] bg-gradient-to-r from-[#110229] to-[#8F90A6] ease-in-out duration-200 absolute -bottom-[2px] left-0"
+						></span>
+						<a href="/">contact us </a>
+					</li>
+				</ul>
+			</div>
+
+			<div class="flex flex-col gap-4 min-w-[300px]">
+				<h1 class=" font-bold text-lg capitalize">subscribe</h1>
+
+				<p class="text-sm font-light leading-relaxed">
+					Subscribe to get latest property, blog news from us
+				</p>
+
+				<div class="flex relative rounded-xl outline outline-[#8F90A6] items-center">
+					<input
+						class="outline-none p-4 w-full active:outline-none focus:outline-none"
+						type="text"
+						placeholder="Email Address"
+					/>
+
+					<div class="absolute h-full flex items-center right-2">
+						<span
+							class="bg-[#1DAEFF] rounded-full p-2 w-10 h-10 aspect-square flex items-center justify-center"
+						>
+							<i class="fa fa-chevron-right text-xl text-white" aria-hidden="true"></i>
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -7,7 +7,7 @@
 				<span class="text-lg lg:text-2xl font-light"> S&G Estate. </span>
 			</div>
 
-			<ul class="hidden lg:flex gap-8 items-center capitalize font-light text-lg lg:text-xl">
+			<ul class="hidden lg:flex gap-8 items-center uppercase font-light text-lg lg:text-xl">
 				<li class="relative group">
 					<span
 						class="group-hover:w-full w-0 h-[2px] bg-gradient-to-r from-[#110229] to-[#8F90A6] ease-in-out duration-200 absolute -bottom-[2px] left-0"
