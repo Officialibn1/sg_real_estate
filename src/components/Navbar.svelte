8687d68d4e6f015e51e3f8db120db0311cced199
Isah Ibn Muhammad <officialibn001@gmail.com>
2024-05-07 01:19:44 +0100

    Completed desktop navbar

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,48 @@
+<nav>
+	<div class="section_container">
+		<div class="flex justify-between items-center w-full">
+			<div class="flex gap-3 items-center">
+				<i class="fa fa-home text-2xl lg:text-5xl font-bold" aria-hidden="true"></i>
+
+				<span class="text-lg lg:text-2xl font-light"> S&G Estate. </span>
+			</div>
+
+			<ul class="hidden lg:flex gap-8 items-center capitalize font-light text-lg lg:text-xl">
+				<li class="relative group">
+					<span
+						class="group-hover:w-full w-0 h-[2px] bg-gradient-to-r from-[#110229] to-[#8F90A6] ease-in-out duration-200 absolute -bottom-[2px] left-0"
+					></span>
+					<a href="/">Home</a>
+				</li>
+				<li class="relative group">
+					<span
+						class="group-hover:w-full w-0 h-[2px] bg-gradient-to-r from-[#110229] to-[#8F90A6] ease-in-out duration-200 absolute -bottom-[2px] left-0"
+					></span>
+					<a href="/">Properties</a>
+				</li>
+				<li class="relative group">
+					<span
+						class="group-hover:w-full w-0 h-[2px] bg-gradient-to-r from-[#110229] to-[#8F90A6] ease-in-out duration-200 absolute -bottom-[2px] left-0"
+					></span>
+					<a href="/">Agents</a>
+				</li>
+				<li class="relative group">
+					<span
+						class="group-hover:w-full w-0 h-[2px] bg-gradient-to-r from-[#110229] to-[#8F90A6] ease-in-out duration-200 absolute -bottom-[2px] left-0"
+					></span>
+					<a href="/">About Us</a>
+				</li>
+				<li class="relative group">
+					<span
+						class="group-hover:w-full w-0 h-[2px] bg-gradient-to-r from-[#110229] to-[#8F90A6] ease-in-out duration-200 absolute -bottom-[2px] left-0"
+					></span>
+					<a href="/">Blogs</a>
+				</li>
+			</ul>
+
+			<div class="flex lg:hidden">
+				<i class="fa fa-bars text-2xl" aria-hidden="true"></i>
+			</div>
+		</div>
+	</div>
+</nav>
