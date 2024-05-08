41276be163519b98cdf24805222e7e9ef4117ae3
Isah Ibn Muhammad <officialibn001@gmail.com>
2024-05-08 22:43:36 +0100

    Hero component complete

---
+++ src/components/Hero.svelte
@@ -0,0 +1,44 @@
+<script>
+</script>
+
+<div class="section_container">
+	<div class="relative w-full overflow-hidden rounded-3xl">
+		<div class="lg:hidden flex flex-col gap-5 bg-[#ecf1f8]">
+			<div class="flex flex-col gap-3 px-7 py-5 md:px-10 md:py-8">
+				<h1
+					class="text-4xl md:text-6xl font-semibold leading-relaxed md:leading-[1.635] hero_text_font"
+				>
+					Easy way to find a perfect property
+				</h1>
+
+				<p class="text-lg md:text-2xl font-light leading-relaxed opacity-80">
+					We provide a complete service for the sale, purchase or rental of real estate.
+				</p>
+			</div>
+
+			<img class="w-full" src="/MobileBanner.svg" alt="Hero Banner" />
+		</div>
+
+		<div class="w-full relative hidden lg:flex">
+			<div class="absolute top-0 left-0 w-1/2 flex flex-col gap-3 pl-16 py-12">
+				<h1
+					class="text-4xl xl:text-6xl font-medium leading-relaxed hero_text_font xl:leading-[1.35] xl:mb-8 xl:pt-20 xl:pl-12"
+				>
+					Easy way to find a perfect property
+				</h1>
+
+				<p class="text-xl xl:text-2xl font-light leading-relaxed opacity-80 xl:pl-12">
+					We provide a complete service for the sale, purchase or rental of real estate.
+				</p>
+			</div>
+
+			<img class="w-full" src="/RSBanner.svg" alt="Hero Banner" />
+		</div>
+	</div>
+</div>
+
+<style>
+	.hero_text_font {
+		font-family: 'Montserrat', sans-serif;
+	}
+</style>
