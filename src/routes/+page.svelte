d2316f4dd98396a705801d5518e59a51956718f3
Isah Ibn Muhammad <officialibn001@gmail.com>
2024-05-09 08:05:57 +0100

    Import recently added section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script>
 	import AvailableStates from '../components/AvailableStates.svelte';
 	import Hero from '../components/Hero.svelte';
+	import RecentlyAdded from '../components/RecentlyAdded.svelte';
 </script>
 
 <Hero />
 
 <AvailableStates />
+
+<RecentlyAdded />
