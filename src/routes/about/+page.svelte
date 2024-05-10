07c69d1e50018acf37e8b53e33a218bc92ba3816
Isah Ibn Muhammad <officialibn001@gmail.com>
2024-05-10 18:51:08 +0100

    About page complete

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,120 @@
-<script>
-	import Hero from '../../components/Hero.svelte';
-</script>
+<div class="section_container">
+	<div class="flex w-full flex-col gap-8">
+		<div
+			class="flex flex-col sm:flex-row justify-between bg-gradient-to-b from-[#C3DFED] to-[#DFF0F7] rounded-lg w-full py-20"
+		>
+			<h1 class="text-5xl font-medium text-center w-full">About Us</h1>
+		</div>
 
-<Hero />
+		<div class="flex flex-col gap-8 w-full md:max-w-[700px] md:mx-auto">
+			<h1 class="about_us_title">About Us: Building Dreams, Building Nigeria</h1>
+
+			<p class="about_us_paragraph">
+				At S&G Estate, we're passionate about creating exceptional living spaces in Nigeria. Founded
+				in 2021, we've established ourselves as a leading real estate developer, transforming Jabi,
+				Wuse, and other sought-after neighborhoods with over 50 completed projects and boasting over
+				100 houses and estates currently in our portfolio.
+			</p>
+
+			<p class="about_us_paragraph">
+				<span class="font-medium">Our Vision:</span> We envision a city where people can find their dream
+				home, a place that fosters connection, comfort, and a luxurious lifestyle.
+			</p>
+
+			<p class="about_us_paragraph">
+				<span class="font-medium">Our Mission:</span> We are committed to delivering quality residences
+				that cater to a diverse range of needs and aspirations. From elegant estates to modern duplexes
+				and stunning lake houses, we offer properties that seamlessly blend functionality with sophisticated
+				design.
+			</p>
+
+			<h1 class="about_us_title">Our Core Values:</h1>
+
+			<ul class="flex flex-col gap-5 list-disc pl-3">
+				<li>
+					<p class="about_us_paragraph">
+						<span class="font-medium">Innovation:</span> We constantly explore new architectural concepts
+						and technologies to create homes that are not just beautiful but also sustainable and efficient.
+					</p>
+				</li>
+				<li>
+					<p class="about_us_paragraph">
+						<span class="font-medium">Integrity:</span> We believe in building trust with our clients
+						by upholding the highest ethical standards and transparency in all our dealings.
+					</p>
+				</li>
+				<li>
+					<p class="about_us_paragraph">
+						<span class="font-medium">Customer Focus:</span> We prioritize understanding your unique
+						needs and preferences to ensure you find the perfect property for your lifestyle.
+					</p>
+				</li>
+			</ul>
+
+			<h1 class="about_us_title">Why Choose S&G Estate?</h1>
+
+			<ul class="flex flex-col gap-5 list-disc pl-3">
+				<li>
+					<p class="about_us_paragraph">
+						<span class="font-medium">Proven Track Record:</span> With over 50 successfully completed
+						projects, we have a wealth of experience in delivering exceptional living spaces.
+					</p>
+				</li>
+				<li>
+					<p class="about_us_paragraph">
+						<span class="font-medium">Diverse Portfolio:</span> We offer a wide range of properties to
+						suit your taste and budget, from spacious estates to intimate duplexes and waterfront havens.
+					</p>
+				</li>
+				<li>
+					<p class="about_us_paragraph">
+						<span class="font-medium">Unwavering Commitment:</span> We are dedicated to providing exceptional
+						customer service throughout the entire buying process and beyond.
+					</p>
+				</li>
+			</ul>
+
+			<h1 class="about_us_title">Ready to Find Your Dream Home?</h1>
+
+			<p class="about_us_paragraph">
+				Browse our listings or contact us today to discuss your needs. Our team of experienced
+				professionals is here to guide you on your real estate journey in Abuja
+			</p>
+
+			<h1 class="about_us_title">In addition to the above, you can consider including:</h1>
+
+			<ul class="flex flex-col gap-3 list-disc pl-3">
+				<li>
+					<p class="about_us_paragraph_bold">
+						A high-quality image showcasing one of your completed projects.
+					</p>
+				</li>
+				<li>
+					<p class="about_us_paragraph_bold">
+						A brief bio about the company founders or leadership team.
+					</p>
+				</li>
+				<li>
+					<p class="about_us_paragraph_bold">Client testimonials praising your work.</p>
+				</li>
+				<li>
+					<p class="about_us_paragraph_bold">Awards and recognitions your company has received.</p>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+<style>
+	.about_us_title {
+		@apply text-2xl lg:text-3xl font-medium mt-12;
+	}
+
+	.about_us_paragraph {
+		@apply text-lg font-light text-justify;
+	}
+
+	.about_us_paragraph_bold {
+		@apply text-lg font-medium text-justify;
+	}
+</style>
