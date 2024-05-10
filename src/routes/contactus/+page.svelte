cc5d42908c6c0719dd11fb9034036149b1dc1a50
Isah Ibn Muhammad <officialibn001@gmail.com>
2024-05-10 18:51:28 +0100

    Contact page created

---
+++ src/routes/contactus/+page.svelte
@@ -0,0 +1,42 @@
+<div class="section_container">
+	<div class="flex w-full flex-col gap-8">
+		<div
+			class="flex flex-col sm:flex-row justify-between bg-gradient-to-b from-[#C3DFED] to-[#DFF0F7] rounded-lg w-full py-20"
+		>
+			<h1 class="text-5xl font-medium text-center w-full">
+				<i class="fa-regular fa-address-book"></i> Contact Us
+			</h1>
+		</div>
+
+		<div class="flex flex-col gap-8 w-full md:max-w-[700px] md:mx-auto">
+			<h1 class="about_us_title">Have a Question or Ready to Get Started?</h1>
+
+			<p class="about_us_paragraph">
+				We're here to help! Whether you have a question about a specific property, want to discuss
+				financing options, or are ready to schedule a viewing, feel free to reach out to us using
+				the form below.
+			</p>
+
+			<p class="about_us_paragraph">
+				Our friendly and knowledgeable team is eager to assist you in finding your dream home in
+				Abuja.
+			</p>
+
+			<p class="about_us_paragraph_bold">We look forward to hearing from you!</p>
+		</div>
+	</div>
+</div>
+
+<style>
+	.about_us_title {
+		@apply text-2xl lg:text-3xl font-medium mt-12;
+	}
+
+	.about_us_paragraph {
+		@apply text-lg font-light text-justify;
+	}
+
+	.about_us_paragraph_bold {
+		@apply text-lg font-medium text-justify;
+	}
+</style>
