68f6f11dfc096e40592f6c13a2619024773a81b3
Isah Ibn Muhammad <officialibn001@gmail.com>
2024-05-10 18:51:20 +0100

    Properties page created

---
+++ src/routes/properties/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+	import AvailableStates from '../../components/AvailableStates.svelte';
+	import ContactUsBanner from '../../components/ContactUsBanner.svelte';
+	import RecentlyAdded from '../../components/RecentlyAdded.svelte';
+</script>
+
+<div
+	class="flex flex-col sm:flex-row justify-between bg-gradient-to-b from-[#C3DFED] to-[#DFF0F7] rounded-lg w-full py-20 px-3"
+>
+	<h1 class="text-4xl md:text-5xl font-medium text-center w-full leading-relaxed">
+		Let's help you find your
+		<span class="font-semibold">Dream Home</span> <i class="fa-regular fa-building"></i>
+	</h1>
+</div>
+
+<div class="section_container flex w-full justify-center">
+	<div class="md:w-1/2 mx-auto">
+		<p class="text-xl md:text-2xl font-light text-center leading-relaxed">
+			Browse our listings of properties or <a
+				href="/contactus"
+				class="text-blue-300 hover:underline">contact us</a
+			>
+			today to discuss your needs. Our team of experienced professionals is here to guide you on your
+			real estate journey in Nigeria
+		</p>
+	</div>
+</div>
+
+<AvailableStates />
+
+<RecentlyAdded />
+
+<ContactUsBanner />
