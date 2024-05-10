ded025406d15fb501144fa4292f02a565c314e16
Isah Ibn Muhammad <officialibn001@gmail.com>
2024-05-10 17:28:54 +0100

    Completed mobile nav

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,5 +1,14 @@
+<script>
+	import { showNav } from '../store';
+	import { fade, scale } from 'svelte/transition';
+
+	const toggleNav = (href) => {
+		$showNav = false;
+	};
+</script>
+
 <nav>
-	<div class="section_container">
+	<div class="section_container bg-white">
 		<div class="flex justify-between items-center w-full">
 			<div class="flex gap-3 items-center">
 				<i class="fa fa-home text-2xl lg:text-5xl font-bold" aria-hidden="true"></i>
@@ -30,7 +39,7 @@
 					<span
 						class="group-hover:w-full w-0 h-[2px] bg-gradient-to-r from-[#110229] to-[#8F90A6] ease-in-out duration-200 absolute -bottom-[2px] left-0"
 					></span>
-					<a href="/">About Us</a>
+					<a href="/about">About Us</a>
 				</li>
 				<li class="relative group">
 					<span
@@ -40,9 +49,55 @@
 				</li>
 			</ul>
 
-			<div class="flex lg:hidden">
+			<button class="flex lg:hidden px-2" on:click={() => ($showNav = true)}>
 				<i class="fa fa-bars text-2xl" aria-hidden="true"></i>
-			</div>
+			</button>
+
+			{#if $showNav}
+				<div
+					transition:scale
+					class={'w-full  fixed right-0 top-0  h-full z-30 bg-white flex flex-col gap-8 p-8 lg:hidden'}
+				>
+					<div class="flex">
+						<button class=" px-2" on:click={() => ($showNav = false)}>
+							<i class="fa fa-x text-2xl" aria-hidden="true"></i>
+						</button>
+					</div>
+					<ul class="w-full flex flex-col lg:hidden gap-10 items-end uppercase font-light text-lg">
+						<li class="">
+							<a href="/" on:click={() => toggleNav()}>Home</a>
+						</li>
+						<li class="">
+							<a href="/" on:click={() => toggleNav()}>Properties</a>
+						</li>
+						<li class="">
+							<a href="/" on:click={() => toggleNav()}>Agents</a>
+						</li>
+						<li class="">
+							<a href="/about" on:click={() => toggleNav()}>About Us</a>
+						</li>
+						<li class="">
+							<a href="/" on:click={() => toggleNav()}>Blogs</a>
+						</li>
+					</ul>
+				</div>
+			{/if}
 		</div>
 	</div>
 </nav>
+
+<!-- <style>
+	.show_nav_animation {
+		animation-name: animate_nav;
+		animation-duration: 500ms;
+	}
+
+	@keyframes animate_nav {
+		from {
+			opacity: 0;
+		}
+		to {
+			opacity: 1;
+		}
+	}
+</style> -->
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,24 @@
 	import '../app.css';
 	import Footer from '../components/Footer.svelte';
 	import Navbar from '../components/Navbar.svelte';
+
+	import { fade, scale } from 'svelte/transition';
+
+	let y;
+
+	$: outerHeight = 0;
 </script>
 
 <Navbar />
 
+{#if y > outerHeight}
+	<div in:scale out:fade={{ duration: 400 }} class="fixed top-0 left-0 z-20 w-full shadow-lg">
+		<Navbar />
+	</div>
+{/if}
+
 <slot />
 
 <Footer />
+
+<svelte:window bind:scrollY={y} bind:outerHeight />
